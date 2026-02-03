2edb7e1116ae809d3d0adaf3f6afc5786be6f0b7
Slsickslider <georgiorocco@outlook.com>
2026-02-03 10:17:33 +0000

    Again fast learning similar syntax, so pretty easy

--- Tests.cpp
+++ Tests.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
 
-int main() {
+int main();
+int Secondary();
+
+int main()
+{
     int a;
     int b; 
     int c;
-    std::string StringTest = "Test String Output";
+    std::string Opperation = "";
 
 
     std::cout << "What is the value of a? ";
@@ -13,10 +17,49 @@ int main() {
 
     std::cout << "What is the value of b? ";
     std::cin >> b;
-    
-    c = a + b;
-    std::cout << "The value of c is: " << c << std::endl;
 
-    std::cout << StringTest << std::endl;
+    std::cout << "What opperation would you like to perform? (+, -, *, /) ";
+    std::cin >> Opperation;
+
+    if (Opperation == "+") 
+    {
+        c = a + b;
+    }
+    else if (Opperation == "-") 
+    {
+        c = a - b;
+    }
+    else if (Opperation == "*") 
+    {
+        c = a * b;
+    }
+    else if (Opperation == "/") 
+    {
+        c = a / b;
+    }
+
+    
+    std::cout << "The value of c is: " << c << std::endl;
+    std::cout << "The opperation performed was: " << Opperation << std::endl;
+
+    Secondary();
+
+    return 0;
+}
+
+int Secondary()
+{
+    int Lists[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for (int i = 0; i < 10; i++) 
+    {
+        std::cout << "List item " << i << " is: " << Lists[i] << std::endl;
+    }
+
+    for (int item : Lists) 
+    {
+        std::cout << "Foreach List item is: " << item << std::endl;
+    }
+
     return 0;
 }
