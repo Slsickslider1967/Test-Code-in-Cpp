9f6d2d3934eea3004208001e2bd7c58aaa957f32
Slsickslider <georgiorocco@outlook.com>
2026-02-03 00:55:55 +0000

    very simialr to C# in the way that it isnt, syntax is very diffrent, but kowing c# is a massive help and bost as i am quickly picking up and learning it all

--- Tests.cpp
+++ Tests.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <string>
 
 int main() {
     int a;
     int b; 
     int c;
+    std::string StringTest = "Test String Output";
+
+
     std::cout << "What is the value of a? ";
     std::cin >> a;
 
@@ -12,5 +16,7 @@ int main() {
     
     c = a + b;
     std::cout << "The value of c is: " << c << std::endl;
+
+    std::cout << StringTest << std::endl;
     return 0;
 }
